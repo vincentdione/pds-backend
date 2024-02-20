a0776757d74e6c4df960d456b6ec2142b60dad65
vincentdione <38090525+vincentdione@users.noreply.github.com>
2024-02-20 20:07:07 +0000

    Update auth.config.js

--- config/auth.config.js
+++ config/auth.config.js
@@ -2,7 +2,7 @@ module.exports = {
   secret: "videkoSecret",
   pgUser:process.env.DB_USER || "postgres",
   pgHost: process.env.DB_HOST || "localhost",
-  pgDatabase: process.env.DB_DATABASE ||  "pds",
+  pgDatabase: process.env.DB_DATABASE ||  "fncl",
   pgPassword: process.env.DB_PASSWORD ||  "Ouvrir1992",
   pgPort: 5432
 };
